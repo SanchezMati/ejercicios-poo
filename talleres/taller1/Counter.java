a763a9c4a9aed7277b43502e508e6acd2e323fd8
PC <msancheznovelli@itba.edu.ar>
2025-03-12 22:08:25 -0300

    "Taller 1, ejercicio 2"

---
+++ talleres/taller1/Counter.java
@@ -0,0 +1,19 @@
+public class Counter
+{
+    private int count;
+
+    public void increment()
+    {
+        count++;
+    }
+
+    public void decrement()
+    {
+        count--;
+    }
+
+    public int getCount()
+    {
+        return count;
+    }
+}
